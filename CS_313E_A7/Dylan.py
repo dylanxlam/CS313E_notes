4ee62a33badbdd7d6b328f6f7f73d5faca66bf4c
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-04 15:38:49 -0500

    update

---
+++ CS_313E_A7/Dylan.py
@@ -0,0 +1,88 @@
+#  File: Work.py 
+
+#  Description:  
+
+#  Student Name:  
+
+#  Student UT EID:  
+
+#  Course Name: CS 313E
+
+#  Unique Number: 
+
+#  Date Created:
+
+#  Date Last Modified:
+
+import sys, time
+
+# FINDING V
+# 
+
+def min_v(n: int, k: int):
+    
+    
+
+def lines_before_sleep(n, k):
+    total_lines = 0
+    for pow in range(2,11):
+        while n // k != 0:
+            total_lines += v // (k ** pow)
+
+
+
+
+
+
+
+# Input: int n, the number of lines of code to write
+#        int k, the productivity factor
+# Output: the number of lines of code that must be 
+#         written before the first cup of coffee
+def linear_search(n: int, k: int) -> int:   
+    # use linear search here
+    n -= 
+    for i in range(n): 
+
+
+
+    return 0 # placeholder
+
+# Input: int n, the number of lines of code to write
+#        int k, the productivity factor
+# Output: the number of lines of code that must be 
+#         written before the first cup of coffee
+def binary_search (n: int, k: int) -> int:
+    # use binary search here
+
+    return 0 # placeholder
+
+# main has been completed for you
+# do NOT change anything below this line
+def main():
+    num_cases = int((sys.stdin.readline()).strip())
+
+    for i in range(num_cases):
+        inp = (sys.stdin.readline()).split()
+        n = int(inp[0])
+        k = int(inp[1])
+
+        start = time.time()
+        print("Binary Search: " + str(binary_search(n, k)))
+        finish = time.time()
+        print("Time: " + str(finish - start))
+
+        print()
+
+        start = time.time()
+        print("Linear Search: " + str(linear_search(n, k)))
+        finish = time.time()
+        print("Time: " + str(finish - start))
+
+        print()
+        print()
+
+# The line above main is for grading purposes only.
+# DO NOT REMOVE THE LINE ABOVE MAIN
+if __name__ == "__main__":
+  main()
