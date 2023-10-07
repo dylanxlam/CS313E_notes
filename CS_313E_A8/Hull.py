d0db8a8436a9abdf5d4dddd385f971414d9d74bb
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-06 22:03:50 -0500

    Update Hull.py

--- CS_313E_A8/Hull.py
+++ CS_313E_A8/Hull.py
@@ -21,103 +21,70 @@
 
 #  Date Last Modified: 10/6/2023
 
-
 import sys
-import math
 
 
-class Point(object):
-    def __init__(self, x=0, y=0):
+class Point:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def dist(self, other):
-        return math.hypot(self.x - other.x, self.y - other.y)
-
-    def __str__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
-
-    def __eq__(self, other):
-        tol = 1.0e-8
-        return ((abs(self.x - other.x) < tol) and (abs(self.y - other.y) < tol))
-
     def __lt__(self, other):
-        if self.x == other.x:
-            return self.y < other.y
-        return self.x < other.x
+        if self.x != other.x:
+            return self.x < other.x
+        return self.y < other.y
 
-    def __le__(self, other):
-        if self.x == other.x:
-            return self.y <= other.y
-        return self.x <= other.x
+def cross(o, a, b):
+    return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x)
 
-    # Add other comparison methods (__ne__, __gt__, __ge__) here.
+def convex_hull(points):
+    points.sort(key=lambda p: (p.x, p.y))  # Sort points lexicographically
+    lower = []
+    upper = []
 
-# Rest of the code remains the same...
+    for point in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], point) <= 0:
+            lower.pop()
+        lower.append(point)
 
+    for point in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], point) <= 0:
+            upper.pop()
+        upper.append(point)
 
-def det(p, q, r):
-    return (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x)
+    return lower[:-1] + upper[:-1]  # Combine lower and upper hulls (exclude duplicates)
 
-def convex_hull(sorted_points):
-    upper_hull = [sorted_points[0], sorted_points[1]]
-    
-    for i in range(2, len(sorted_points)):
-        upper_hull.append(sorted_points[i])
-        while len(upper_hull) >= 3 and det(upper_hull[-3], upper_hull[-2], upper_hull[-1]) < 0:
-            del upper_hull[-2]
-    
-    lower_hull = [sorted_points[-1], sorted_points[-2]]
-    
-    for i in range(len(sorted_points) - 3, -1, -1):
-        lower_hull.append(sorted_points[i])
-        while len(lower_hull) >= 3 and det(lower_hull[-3], lower_hull[-2], lower_hull[-1]) < 0:
-            del lower_hull[-2]
-    
-    # Remove first and last points from lower hull to avoid duplication
-    lower_hull.pop(0)
-    lower_hull.pop(-1)
-    
-    # Combine upper and lower hulls to form the convex hull
-    convex_hull = upper_hull + lower_hull
-    return convex_hull
+def area_of_polygon(polygon):
+    n = len(polygon)
+    if n < 3:
+        return 0.0
 
-def area_poly(convex_poly):
-    # Compute and return the area of a convex polygon
-    det_sum = 0
-    n = len(convex_poly)
+    area = 0.0
     for i in range(n):
-        det_sum += convex_poly[i].x * convex_poly[(i + 1) % n].y
-        det_sum -= convex_poly[i].y * convex_poly[(i + 1) % n].x
-    area = 0.5 * abs(det_sum)
+        j = (i + 1) % n
+        area += polygon[i].x * polygon[j].y
+        area -= polygon[i].y * polygon[j].x
+
+    area = abs(area) / 2.0
     return area
 
 def main():
+    n = int(sys.stdin.readline())
     points_list = []
-    
-    num_points = int(sys.stdin.readline().strip())
-    
-    for i in range(num_points):
-        line = sys.stdin.readline().strip().split()
-        x, y = int(line[0]), int(line[1])
-        points_list.append(Point(x, y))
-    
-    sorted_points = sorted(points_list)
-    
-    convex_hull_points = convex_hull(sorted_points)
-    
-    # Sort the convex hull points based on their coordinates
-    convex_hull_points.sort(key=lambda p: (p.x, p.y))
-    
-    # Print the vertices of the convex hull in the correct order
-    print("Convex Hull")
-    for point in convex_hull_points:
-        print(point)
-    
-    area = area_poly(convex_hull_points)
-    print("\nArea of Convex Hull =", area)
 
+    for _ in range(n):
+        x, y = map(int, sys.stdin.readline().split())
+        points_list.append(Point(x, y))
+
+    hull = convex_hull(points_list)
+    hull.sort(key=lambda p: (p.x, p.y))  # Sort points in convex hull lexicographically
+
+    print("Convex Hull")
+    for point in hull:
+        print(f"({point.x}, {point.y})")
+
+    hull_area = area_of_polygon(hull)
+    print(f"\nArea of Convex Hull = {hull_area:.1f}")
 
 if __name__ == "__main__":
     main()
-
