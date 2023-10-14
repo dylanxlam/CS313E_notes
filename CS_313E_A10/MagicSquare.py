05ae75a8d3e08677c22c909e2656abde99a378ce
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-13 19:35:31 -0500

    Create MagicSquare.py

---
+++ CS_313E_A10/MagicSquare.py
@@ -0,0 +1,81 @@
+# File: MagicSquare.py
+
+# Description:
+
+# Student's Name: Alexander Romero-Barrionuevo
+
+# Student's UT EID: ANR3784
+
+# Partner's Name: Dylan Lam
+
+# Partner's UT EID: DXL85
+
+# Course Name: CS 313E
+
+# Unique Number: 52605
+
+# Date Created: 10/13/2023
+
+# Date Last Modified: 10/13/2023
+
+# Input: 1-D list of integers a
+# Output: returns True if this list is a magic square
+# or False otherwise
+def is_magic(a):
+    # Establish magic constant
+    magic_constant = 3 * (((3**2) + 1) / 2)
+
+    # Check for each row, column, and diagonals
+    # in which sums are compared to magic constant
+    for row in range(0, 9, 3):
+        if sum(a[row:row+3]) != magic_constant:
+            return False
+    for column in range(3):
+        if sum(a[column::3]) != magic_constant:
+            return False
+    if a[0] + a[4] + a[8] != magic_constant:
+        return False
+    if a[2] + a[4] + a[6] != magic_constant:
+        return False
+
+    # return True as all cases pass
+    return True
+
+# Input: 1-D list of integers a and an index idx
+# Output: prints only those permutations that are magic
+def permute(a, idx):
+    # Check if all permutations are complete
+    if idx == 9:
+        if is_magic(a):
+            print_square(a)
+        return
+
+    for i in range(idx, 9):
+        a[idx], a[i] = a[i], a[idx]
+        permute(a, idx + 1)
+        a[idx], a[i] = a[i], a[idx]
+
+# Input: 1-D list of integers a
+# Output: prints this as a 2-D list
+def print_square(a):
+    square = reshape(a)
+    for row in square:
+        print("\t".join(map(str, row)))  # Use "\t" to separate elements, and remove the extra print() line
+    print()  # Add a single print() to separate the squares
+
+
+# Input: 1-D list of integers a
+# Output: returns a 2-D list
+def reshape(a):
+    size = int(len(a) ** 0.5)
+    return [a[i:i+size] for i in range(0, len(a), size)]
+
+def main():
+    # create a 1-D list of numbers from 1 to 9
+    nums = list(range(1, 10))
+
+    # call permute to get all 3x3 magic squares
+    permute(nums, 0)
+
+if __name__ == "__main__":
+    main()
