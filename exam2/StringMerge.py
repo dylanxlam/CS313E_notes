efc059bf2b85401ba787b6ffffbd722816faae08
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-27 20:26:07 -0500

    continue

---
+++ exam2/StringMerge.py
@@ -0,0 +1,36 @@
+# File: StringMerge.py
+# Description: Determine how many new strings can be formed according to the criteria given using recursion.
+# Student Name:
+# Student UT EID:
+# Course Name: CS 313E
+# Unique Number:
+
+import sys
+
+# Input: 2 strings, s1 and s2, which both have length >= 0
+# Output: the number of possible new strings that can be formed by merging s1 and s2
+def stringMerge(s1, s2):
+    if len(s1) == 0 and len(s2) == 0:
+        return 1
+    
+    count = 0
+
+    if len(s1) > 0:
+        count = count + stringMerge(s1[1:], s2)
+        
+    if len(s2) > 0:
+        count = count + stringMerge(s1, s2[1:])
+
+    return count
+
+def main():
+    # read in 2 input strings
+    s1 = sys.stdin.readline().strip()
+    s2 = sys.stdin.readline().strip()
+
+    # find all new strings
+    result = stringMerge(s1, s2)
+    print(result)
+
+if __name__ == '__main__':
+    main()
