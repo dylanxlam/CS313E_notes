8dce8e966e0fc77d69fae2b9d0e44d8f1f69fc89
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-27 19:15:57 -0500

    Update Div13.py

--- exam2/Div13.py
+++ exam2/Div13.py
@@ -44,7 +44,7 @@ def div(n):
         return min_step_count
 
     result = min_steps(n)
-    print(result * result) if result != float('inf') else -1
+    print(result * result) 
 
 
 if __name__ == "__main__":
