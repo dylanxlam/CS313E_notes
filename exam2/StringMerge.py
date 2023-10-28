448ce8b938461d7a26f2069f2ca8b1f10f9582d6
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-27 20:33:08 -0500

    Update StringMerge.py

--- exam2/StringMerge.py
+++ exam2/StringMerge.py
@@ -13,6 +13,9 @@ def stringMerge(s1, s2):
     if len(s1) == 0 or len(s2) == 0:
         return 1
     
+    if s1[0] == s2[0]:
+        count -= 1
+    
     count = 0
 
     count = count + stringMerge(s1[1:], s2)
