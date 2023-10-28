9dbcd1912c45f0b5abd3aca0b155e9f1bb2f2403
dvlanl <126316930+dylanxlam@users.noreply.github.com>
2023-10-27 21:13:26 -0500

    Create Purchase.py

---
+++ exam2/Purchase.py
@@ -0,0 +1,41 @@
+# File: Purchase.py
+# Description: Remove purchase requests to make a list of requests valid
+
+# Student Name: 
+# Student UT EID: 
+# Course Name: CS 313E
+# Unique Number: 52605
+
+import sys
+
+# Input: a string s, where each letter represents a request to purchase a particular item
+# Output: s with characters removed such that there is only one request per item. The output 
+# should be the alphabetically smallest string meeting these constraints
+def filter_requests(s):
+    stack = []
+    last_idx = {character: -1 for character in s}
+
+    for i, character in enumerate(s):
+        if i < last_idx[character]:
+            continue
+
+        
+        while len(stack) > 0 and stack[-1] > character and s.find(stack[-1], i) != -1:
+            removed = stack.pop()
+            last_idx[removed] = s.find(removed, i)
+            
+        if character not in stack:
+            stack.append(character)
+
+
+    output = ''.join(stack)
+    return output
+
+def main():
+    request = sys.stdin.readline().strip()
+    print(filter_requests(request))
+
+if __name__ == "__main__":
+    main()
+
+
